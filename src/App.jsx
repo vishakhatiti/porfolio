9908dee29395540860913374c53e80819659a0a2
vishakhatiti <vishakhasm1234@gmail.com>
2026-04-30 18:32:06 +0530

    React portfolio setup (Vite) - initial structure

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 style={{ textAlign: "center", marginTop: "40px" }}>
+        Portfolio Coming Soon 🚀
+      </h1>
+    </>
+  );
+}
+
+export default App;
